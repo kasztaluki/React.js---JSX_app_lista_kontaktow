d4f2e7523885655c55849fc57aa2b6f528e4da08
Łuki Kasztaniewicz <grabowski.lukasz.88@gmail.com>
2018-05-15 22:52:51 +0200

    Change app.js

--- components/App.js
+++ components/App.js
@@ -30,7 +30,7 @@ var App = React.createClass({
     return (
       <div className={'app'}>
         <ContactForm contact={contactForm} />
-        <Contacts items={contact} />
+        <Contacts items={contacts} />
       </div>
     );
   }
